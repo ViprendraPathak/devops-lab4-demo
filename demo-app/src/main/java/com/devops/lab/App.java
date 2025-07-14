bed23444b14526c195602979a5d39a22bff6646d
Viprendra Pathak <2021wa86151@wilp.bits-pilani.ac.in>
2025-07-14 18:11:58 +0530

    Updated greeting message

--- demo-app/src/main/java/com/devops/lab/App.java
+++ demo-app/src/main/java/com/devops/lab/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello By VIP" );
+        System.out.println( "Hello From VIP!" );
     }
 }
 // New feature added
